292d036da22c0079272c0d1972a9d62835070d75
James Ingram <mr.cool.jamz@gmail.com>
2021-05-24 11:20:51 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'dalle-pytorch',
+  packages = find_packages(),
+  include_package_data = True,
+  version = '0.12.0',
+  license='MIT',
+  description = 'DALL-E - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/dalle-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers',
+    'text-to-image'
+  ],
+  install_requires=[
+    'axial_positional_embedding',
+    'DALL-E',
+    'einops>=0.3',
+    'ftfy',
+    'g-mlp-pytorch',
+    'pillow',
+    'regex',
+    'taming-transformers',
+    'tokenizers',
+    'torch>=1.6',
+    'torchvision',
+    'transformers',
+    'tqdm',
+    'youtokentome'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
